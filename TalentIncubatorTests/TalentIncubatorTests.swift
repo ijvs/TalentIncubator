9bffc925bda09acf7f426c0f67a06ffa70b5fc49
Jonathan Velazquez <jvelazquez@nearsoft.com>
2018-06-11 08:27:24 -0700

    Add test for EventRepository and EventServer

--- TalentIncubatorTests/TalentIncubatorTests.swift
+++ TalentIncubatorTests/TalentIncubatorTests.swift
@@ -11,6 +11,7 @@ import XCTest
 
 class TalentIncubatorTests: XCTestCase {
     
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,16 +22,18 @@ class TalentIncubatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testServerGetEvents() {
+        fetchEvents(eventRepository: EventServer())
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func fetchEvents(eventRepository: EventRepository) {
+        let expectation = XCTestExpectation(description: "Get Events")
+        eventRepository.searchEvents { (events, error) in
+            XCTAssertNotNil(events, "No data receive from the repository")
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 5.0)
     }
     
 }
